a03ce1bedaa344426d1aa1882f2c6fb6b9759b55
jashkarangiya <jashkarangiya@gmail.com>
2023-03-25 23:57:04 +0530

    Updated files

---
+++ server/routes/users.js
@@ -0,0 +1,17 @@
+import { Express, Router } from "express";
+import {get } from "mongoose";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriends,
+
+
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+const router = express.Router();
+
+// Read
+router.get(":/id", verifyToken, getUser);
+router.get(":/id/friends", verifyToken, getUserFriends);
+
+// Update:
