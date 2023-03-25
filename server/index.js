904baa7873f0f8c415288864acf8d76c409dff35
jashkarangiya <jashkarangiya@gmail.com>
2023-03-25 23:42:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
+
 /* File Storage */
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
